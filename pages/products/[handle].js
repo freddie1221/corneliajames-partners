0a9a94d775ddcf5d893a5d628e302c1a78293421
Frederick Lawson <freddie@corneliajames.com>
2024-07-13 14:21:32 +0100

    adds download button

--- pages/products/[handle].js
+++ pages/products/[handle].js
@@ -4,6 +4,8 @@ import { createAdminApiClient } from '@shopify/admin-api-client';
 import '../../app/globals.css';
 import Link from 'next/link';
 import LoadingSpinner from '../../components/LoadingSpinner';
+import DownloadButton from '../../components/DownloadButton';
+import DownloadAll from '../../components/DownloadAll';
 
 export default function Product({ product }) {
   const [loading, setLoading] = useState(false);
@@ -19,51 +21,45 @@ export default function Product({ product }) {
   };
 
   return (
-    <div className="container mx-auto p-10  text-gray-900">
+    <div className="container mx-auto p-10  text-gray-900" >
       {loading && <LoadingSpinner />}
+      
+      {/* main product image and download button */}
       <div className="flex flex-col md:flex-row items-center">
         <img
           src={product.featuredImage?.url || 'No image available'}
           alt={product.title}
           className="w-full md:w-1/2 h-auto object-cover mb-4 md:mb-0 md:mr-4 rounded-lg"
         />
-
-        {product.featuredImage?.url && (
-          <a
-            href={product.featuredImage.url}
-            download={`${product.title.replace(/\s+/g, '_')}.jpg`}
-            className="absolute top-2 right-2 bg-white p-2 rounded-full shadow-md hover:bg-gray-100 transition duration-300"
-            title="Download Image"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-gray-800"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 16v2a2 2 0 002 2h12a2 2 0 002-2v-2M7 10l5 5m0 0l5-5m-5 5V3"
-              />
-            </svg>
-          </a>
-        )}
-
         <div className="md:w-1/2">
-          <h1 className="text-4xl font-serif font-bold mb-4">{product.title}</h1>
-          <p className="text-lg mb-2">Status: {product.status}</p>
-          <a
-            href={`https://www.corneliajames.com/products/${product.handle}`}
-            className="mt-4 inline-block bg-black text-white py-2 px-4 rounded-lg hover:bg-gray-800 transition duration-300"
-          >
-            View Product Detail
-          </a>
-          <Link href="/products" onClick={handleBackClick} className="mt-4 text-black py-2 px-4 rounded-lg hover:bg-gray-100 transition duration-300">
-            ← Back to Products
-          </Link>
+          <div className="flex flex-col space-y-4">
+            <h1 className="text-4xl font-serif font-bold">{product.title}</h1>
+            <a
+              href={`https://www.corneliajames.com/products/${product.handle}`}
+              className="inline-block bg-black text-white py-2 px-4 rounded-lg hover:bg-gray-800 transition duration-300 text-center"
+            >
+              View Product Detail
+            </a>
+            <DownloadAll images={product.images.edges.map(edge => ({ url: edge.node.url, title: product.title }))} />
+            <Link href="/products" onClick={handleBackClick} className="text-black py-2 px-4 rounded-lg hover:bg-gray-100 transition duration-300">
+              ← Back to Products
+            </Link>
+          </div>
+
+        </div>
+      </div>
+      
+      {/* additional product images */}
+      <div className="pt-8"> 
+      <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        {product.images.edges.map(edge => (
+          <img
+            key={edge.node.url}
+            src={edge.node.url}
+            alt={product.title}
+            className="w-full h-auto object-cover rounded-lg"
+          />
+          ))}
         </div>
       </div>
     </div>
@@ -127,6 +123,13 @@ export async function getStaticProps({ params }) {
       status
       featuredImage {
         url
+      }
+      images(first: 10) {
+        edges {
+          node {
+            url
+          }
+        }
       }
     }
   }`;
