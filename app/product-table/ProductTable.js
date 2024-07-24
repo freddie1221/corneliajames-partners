9abccbe38fb9f15d9cd4490a595a8d3df7357a8f
Frederick Lawson <freddie@corneliajames.com>
2024-07-24 15:53:33 -0300

    CSV v1 live

--- app/product-table/ProductTable.js
+++ app/product-table/ProductTable.js
@@ -49,8 +49,6 @@ export default function ProductTable({ initialProducts }) {
     'SKU'
   ];
 
-
-
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Product Table</h1>
