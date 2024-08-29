26c6e384be416cda4772c8ac4ebde19bd0145c8e
Frederick Lawson <freddie@corneliajames.com>
2024-08-29 15:49:57 -0300

    copy change

--- app/page.js
+++ app/page.js
@@ -33,7 +33,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Click here for the Cornelia James Product Data Grid (work in progress)
+            Click here for the Cornelia James Product Data Grid & CSV download link
           </p>
         </a>
 
