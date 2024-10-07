54ae91c5fb565763ddcddb0d3400bf0669d5fd01
Frederick Lawson <freddie@corneliajames.com>
2024-10-07 18:16:30 -0300

    adds mixpanel

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import Header from "../components/header";
 import "./globals.css";
+import MixpanelInitializer from "../lib/mixpanel";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className} >
+        <MixpanelInitializer />
         <Header />
         <main>
           {children}
