811aa69234a65f6d1ddac3a0d8e6215ca21f7807
Frederick Lawson <freddie@corneliajames.com>
2025-01-03 16:34:29 -0400

    fix

--- app/partners/product-table/ProductTable.js
+++ app/partners/product-table/ProductTable.js
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 export default function ProductTable({ initialProducts }) {
   const [products] = useState(initialProducts);
--- app/partners/product-table/page.js
+++ app/partners/product-table/page.js
@@ -1,4 +1,4 @@
-import { getProductsData } from '../../lib/shopify';
+import { getProductsData } from '@/lib/shopify';
 import ProductTable from './ProductTable';
 
 export async function generateStaticParams() {
